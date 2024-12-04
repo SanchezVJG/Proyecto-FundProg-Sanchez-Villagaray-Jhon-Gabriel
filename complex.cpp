98693683bcfe6bef9ebd20c985add7c47e65186d
Jhon Sanchez <jhon.sanchez.v@uni.pe>
2024-12-04 18:53:14 -0500

    Added complex operators

--- complex.cpp
+++ complex.cpp
@@ -5,3 +5,20 @@ using namespace std;
 Complex operator+(Complex C1,Complex C2){
     return Complex {{C1.re+C2.re},{C1.im+C2.im}};
 }
+
+Complex operator*(Complex C1,Complex C2){
+    return Complex {{C1.re*C2.re-C1.im*C2.im},{C1.re*C2.im+C1.im*C2.re}};
+}
+
+Complex exp(Complex C){
+    return Complex {{C.re*cos(C.im)},{C.re*sin(C.im)}};
+}
+
+ostream &operator<<(ostream &os, Complex C){
+    os << C.re;
+    if (C.im >0)
+    os << "+";
+    if (C.im !=0)
+    os << C.im << "*i";
+    return os;
+}
--- complex.h
+++ complex.h
@@ -1,8 +1,11 @@
 #ifndef __COMPLEX_H__
 #define __COMPLEX_H__
+#include <cmath>
 using Real = float;
 struct Complex{
     Real re=0;
     Real im=0;
 };
+
+Complex exp(Complex);
 #endif
